5d67654b1c07766ce2c299e8b4f241efcb09776c
NazimKhak <122981712+NazimKhak@users.noreply.github.com>
2023-01-23 23:17:12 +0200

    cath()
    Problem with cath(), need to resolve

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import './css/styles.css';
+import './sass/index.scss';
+import Notiflix from 'notiflix';
 
 var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
@@ -15,33 +16,66 @@ refs.inputField.addEventListener(
 
 function displayCountries(ev) {
   fetchcountries(ev.target.value);
+  if (ev.target.value === '') {
+    refs.countryLInfo.innerHTML = '';
+    refs.countryList.innerHTML = '';
+  }
 }
 
 function createMarkup(information) {
   const html = information
     .map(element => {
       return `<li class='item'>
-        <img src='${element.flag}' width='100' heigth='100' alt='flag image' />
-        <p>${element.name}</p>
-        <p>${element.capital}</p>
-        <p>${element.languages[0].name}</p>
-      </li>`;
+          <img src='${element.flag}' width='30' heigth='30' alt='flag image' />
+          <p>${element.name}</p>
+          <p>${element.capital}</p>
+          <p>${element.languages[0].name}</p>
+        </li>`;
     })
-    .slice(0, 10)
+    .slice(0, 15)
     .join('');
 
   refs.countryList.innerHTML = html;
+  if (information.length === 1) {
+    const htmloneElement = information
+      .map(element => {
+        return `
+        <div class ='flag_country'>
+        <img src='${element.flag}' width='40' height="30"  alt='flag image' />
+        <h2>${element.name}</h2>
+        </div>
+          <p><span>Capital: </span>${element.capital}</p>
+          <p><span>Population: </span>${element.population}</p>
+          <p><span>Language: </span>${element.languages[0].name}</p>
+       `;
+      })
+      .slice('')
+      .join('');
+    refs.countryLInfo.innerHTML = htmloneElement;
+    refs.countryList.innerHTML = '';
+  }
 }
 
 function fetchcountries(countryName) {
-  fetch(
-    `https://restcountries.com/v2/name/${countryName}?fields=name,flag,capital,languages`
-  )
-    .then(response => {
-      return response.json();
-    })
-    .then(countryInfo => {
-      createMarkup(countryInfo);
-    })
-    .catch(error => {});
+  try {
+    fetch(
+      `https://restcountries.com/v2/name/${countryName}?fields=name,flag,capital,languages,population`
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(countryInfo => {
+        if (countryInfo.length > 10)
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name'
+          );
+        {
+          if (countryInfo.length < 10) {
+            createMarkup(countryInfo);
+          }
+        }
+      });
+  } catch (error) {
+    console.log('ОШИБКа');
+  }
 }
